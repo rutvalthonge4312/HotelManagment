d95962d572f59c1bc493adb03dd3c672705a5fea
rutvalthonge4312 <rutvalthonge4312@gmail.com>
2024-02-21 01:19:45 +0530

    Initial project setup

---
+++ src/AllRoutes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import RouteLogger from './components/RouteLogger';
+import HomePage from './screeens/HomePage';
+
+const AllRoutes = () => {
+  return (
+    <div>
+      <RouteLogger />
+      <Routes>
+      <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default AllRoutes;
